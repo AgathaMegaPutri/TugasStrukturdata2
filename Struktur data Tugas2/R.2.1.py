f1e0eaa78ab1684c80627ca27a4b2a41eee047ba
Agatha Mega Putri <58312337+AgathaMegaPutri@users.noreply.github.com>
2021-06-17 13:21:45 +0700

    Add files via upload

---
+++ Struktur data Tugas2/R.2.1.py
@@ -0,0 +1,5 @@
+# soal:
+# contoh perangkat lunak life-critical
+
+# jawab:
+# aplikasi untuk berkomunikasi jarak jauh, aplikasi maps dan gps untuk melihat lalu lintas , aplikasi cyberlife(aplikasi untuk melacak kapal dan pesawat) pesawat
